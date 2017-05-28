18fd717f4dae27337f55434b9ef8ee5ba0ffc2bd
eashirobokov <egorka13@users.noreply.github.com>
2017-05-28 20:16:32 +0300

    Update Form1.Designer.cs

--- Seti_with_GUI/Form1.Designer.cs
+++ Seti_with_GUI/Form1.Designer.cs
@@ -4,9 +4,6 @@
     {
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
         /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
