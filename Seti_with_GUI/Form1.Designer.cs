58277fdc7702104e9d16f663231af3f8ea2b28c7
Maxim <mvshmakov@mail.ru>
2017-05-28 20:07:39 +0300

    Update Form1.Designer.cs

--- Seti_with_GUI/Form1.Designer.cs
+++ Seti_with_GUI/Form1.Designer.cs
@@ -2,9 +2,6 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Обязательная переменная конструктора.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
